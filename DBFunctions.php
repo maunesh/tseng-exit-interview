025551ad23ff932faa936fcbaca88f16c9d4048b
Joshua David <Joshua@Joshua-David.net>
2013-07-09 13:58:24 -0700

    The survey/database end has full functionality

--- DBFunctions.php
+++ DBFunctions.php
@@ -4,43 +4,52 @@ global $mysqli;
 $mysqli = new mysqli(DLS01_HOSTNAME, DLS01_USERNAME, DLS01_PASSWORD);
 function dbQuery($query) {
     global $mysqli;
+    $debug = false;
+    if($debug) echo '<pre class="query">' . $query . '</pre>';
     $result = mysqli_query($mysqli, $query);
     return $result;
 }
-dbQuery('CREATE SCHEMA IF NOT EXISTS Survey_Monkey_Results');
-dbQuery('USE Survey_Monkey_Results');
-dbQuery('CREATE TABLE IF NOT EXISTS Surveys (
-    ID INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(ID)
-)');
-dbQuery('CREATE TABLE IF NOT EXISTS SurveyResponses (
-    ID INT NOT NULL AUTO_INCREMENT,
-    ConfirmationCode INT,
-    Survey_ID INT,
-    PRIMARY KEY(ID)
-)');
-dbQuery('CREATE TABLE IF NOT EXISTS Questions (
-    ID INT NOT NULL AUTO_INCREMENT,
-    Survey_ID INT,
-    QuestionText VARCHAR(4096),
-    QuestionType VARCHAR(256),
-    QuestionInstructions VARCHAR(4096),
-    PRIMARY KEY(ID)
-)');
-dbQuery('CREATE TABLE IF NOT EXISTS QuestionResponses (
-    ID INT NOT NULL AUTO_INCREMENT,
-    Survey_ID INT,
-    SurveyResponse_ID INT,
-    Question_ID INT,
-    ResponseText VARCHAR(4096),
-    PRIMARY KEY(ID)
-)');
-dbQuery('CREATE TABLE IF NOT EXISTS QuestionChoices (
-    ID INT NOT NULL AUTO_INCREMENT,
-    Question_ID INT,
-    ChoiceText VARCHAR(4096),
-    PRIMARY KEY(ID)
-)');
+function resultsAsArray($mysqli_result) {
+    $arr = array();
+    while($arr[] = mysqli_fetch_assoc($mysqli_result)) {}
+    return $arr;
+}
+function init() {
+    dbQuery('CREATE SCHEMA IF NOT EXISTS Survey_Monkey_Results');
+    dbQuery('USE Survey_Monkey_Results');
+    dbQuery('CREATE TABLE IF NOT EXISTS Surveys (
+        ID INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(ID)
+    )');
+    dbQuery('CREATE TABLE IF NOT EXISTS SurveyResponses (
+        ID INT NOT NULL AUTO_INCREMENT,
+        ConfirmationCode INT,
+        Survey_ID INT,
+        PRIMARY KEY(ID)
+    )');
+    dbQuery('CREATE TABLE IF NOT EXISTS Questions (
+        ID INT NOT NULL AUTO_INCREMENT,
+        Survey_ID INT,
+        QuestionText VARCHAR(4096),
+        QuestionType VARCHAR(256),
+        QuestionInstructions VARCHAR(4096),
+        PRIMARY KEY(ID)
+    )');
+    dbQuery('CREATE TABLE IF NOT EXISTS QuestionResponses (
+        ID INT NOT NULL AUTO_INCREMENT,
+        Survey_ID INT,
+        SurveyResponse_ID INT,
+        Question_ID INT,
+        ResponseText VARCHAR(4096),
+        PRIMARY KEY(ID)
+    )');
+    dbQuery('CREATE TABLE IF NOT EXISTS QuestionChoices (
+        ID INT NOT NULL AUTO_INCREMENT,
+        Question_ID INT,
+        ChoiceText VARCHAR(4096),
+        PRIMARY KEY(ID)
+    )');
+}
 function padNumber($n, $size = 2) {
     $padded = "";
     $padded .= $n;
@@ -78,7 +87,7 @@ function getQuestionType($id, $survey_id) {
 }
 function getQuestionResponseText($question_id, $surveyresponse_id, $qname) {
     $responseText = "";
-    $result = dbQuery("SELECT QuestionText FROM Questions 
+    $result = dbQuery("SELECT ResponseText FROM QuestionResponses 
         WHERE Question_ID={$question_id} 
         AND SurveyResponse_ID={$surveyresponse_id}");
     $row = mysqli_fetch_row($result);
@@ -122,7 +131,8 @@ function questionToHTML($question_id, $survey_id, $surveyresponse_id, $position 
         $HTML .= '<div class="question">';
         $HTML .= $questionText;
         $HTML .= '</div>';
-    } else {
+    } 
+    else {
         return "";
     }
     if($questionInstructions) {
@@ -135,7 +145,39 @@ function questionToHTML($question_id, $survey_id, $surveyresponse_id, $position 
         $HTML .= $questionResponseText;
         $HTML .= '</textarea>';
     }
+    else {
+        if($questionType == "radio") {
+            $result = dbQuery("SELECT ChoiceText FROM QuestionChoices
+                WHERE Question_ID={$question_id};");
+            $rows = resultsAsArray($result);
+            foreach($rows as $index => $row) {
+                $choicetext = $row["ChoiceText"];
+                $savedChoice = $questionResponseText;
+                if(!empty($_POST[$qname])) $savedChoice = $_POST[$qname];
+                if(strlen($choicetext) > 0) {
+                    $HTML .= '<div class="choice">';
+                    $HTML .= '<input type="radio" id="'.$qname.'_'.$index.'" name="'.$qname.'" value="'.$choicetext.'" ';
+                    if($savedChoice == $choicetext) $HTML .= 'checked="checked" ';
+                    $HTML .= '/>';
+                    $HTML .= '<label for="'.$qname.'_'.$index.'">' . $choicetext . '</label>';
+                    $HTML .= '</div>';
+                }
+            }
+        }
+    }
     $HTML .= "</li>";
     return $HTML;
 }
+function getSurveyResponseID($code) {
+    $result = dbQuery("SELECT ID FROM SurveyResponses
+        WHERE ConfirmationCode=\"{$code}\";");
+    if($result) {
+        $rows = resultsAsArray($result);
+        $id = ($rows[0]["ID"]);
+    }
+    else {
+    }
+    return $id;
+}
+init();
 ?>
